0786575daa9d3308938bc6d7668600d26d69fda3
Vivek Gupta <52711458+vivekgupta23@users.noreply.github.com>
2021-07-21 21:41:17 +0530

    Final expense code

---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,27 @@
+import {useState} from "react";
+import ExpensesList from "./ExpensesList";
+import "./Expenses.css";
+import ExpensesFilter from "./ExpensesFilter";
+import Card from "../UI/Card";
+import ExpenseChart from "./ExpensesChart";
+
+function Expenses(props) {
+	const [filteredYear, setFilteredYear] = useState("2021");
+	const filterChangeHandler = (selectedYear) => {
+		setFilteredYear(selectedYear);
+	};
+
+	const filteredExpenses = props.items.filter((newExpenseArray) => newExpenseArray.date.getFullYear().toString() === filteredYear);
+
+	return (
+		<div>
+			<Card className='expenses'>
+				<ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
+				<ExpenseChart expenses={filteredExpenses} />
+				<ExpensesList items={filteredExpenses} />
+			</Card>
+		</div>
+	);
+}
+
+export default Expenses;
